b28e61299bb019e9180dfa0d1a91b2728838f970
Walter <walteracero005@gmail.com>
2025-07-01 16:37:38 -0500

    arreglo

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.repaso_prueba"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
